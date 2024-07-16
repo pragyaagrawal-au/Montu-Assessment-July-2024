90e4ee6eeabfc153ea0cc6602917fdd903cfcbcd
pragyaagrawal.au@gmail.com <pragyaagrawal.au@gmail.com>
2024-07-16 18:29:52 +0000

    optimised code

--- models/final/agg_session_level_metrics.sql
+++ models/final/agg_session_level_metrics.sql
@@ -2,7 +2,7 @@
     materialized='table'
 ) }}
 
-with event_data as (
+with source_data as (
     select
         session_id,
         event_date,
@@ -17,55 +17,28 @@ with event_data as (
     from {{ ref('stg_events') }}
 ),
 
-total_sessions as (
+aggregated_data as (
     select
         event_date,
-        count(distinct session_id) as total_sessions
-    from event_data
-    group by event_date
-),
-
-total_users as (
-    select
-        event_date,
-        count(distinct user_pseudo_id) as total_users
-    from event_data
-    group by event_date
-),
-
-total_new_users as (
-    select
-        event_date,
-        count(distinct user_pseudo_id) as total_new_users
-    from event_data
-    where TIMESTAMP_SECONDS(user_first_touch_timestamp) = TIMESTAMP_SECONDS(event_timestamp)
-    group by event_date
-),
-
-total_page_views as (
-    select
-        event_date,
-        count(*) as total_page_views
-    from event_data
-    where LOWER(event_name) = 'page_view'
-    group by event_date
-),
-
-search_sessions as (
-    select
-        event_date,
-        count(*) as total_sessions_with_search
-    from event_data
-    where LOWER(event_name) = 'view_search_results'
-    group by event_date
+        session_id,
+        user_pseudo_id,
+        user_first_touch_timestamp,
+        event_timestamp,
+        event_name,
+        count(distinct session_id) over (partition by event_date) as total_sessions,
+        count(distinct user_pseudo_id) over (partition by event_date) as total_users,
+        count(distinct case when TIMESTAMP_MICROS(user_first_touch_timestamp) = TIMESTAMP_MICROS(event_timestamp) then user_pseudo_id else null end) over (partition by event_date) as total_new_users,
+        count(case when event_name = 'page_view' then 1 else null end) over (partition by event_date) as total_page_views,
+        count(case when event_name = 'view_search_results' then 1 else null end) over (partition by event_date) as total_sessions_with_search
+    from source_data
 ),
 
 session_durations as (
     select
         event_date,
         user_pseudo_id,
-        MAX(TIMESTAMP_SECONDS(event_timestamp)) - MIN(TIMESTAMP_SECONDS(event_timestamp)) as session_duration
-    from event_data
+        TIMESTAMP_DIFF(MAX(TIMESTAMP_MICROS(event_timestamp)), MIN(TIMESTAMP_MICROS(event_timestamp)), SECOND) as session_duration
+    from source_data
     group by event_date, user_pseudo_id
 ),
 
@@ -78,16 +51,13 @@ average_session_duration as (
 )
 
 select
-    ts.event_date,
-    ts.total_sessions,
-    tu.total_users,
-    tnu.total_new_users,
-    tpv.total_page_views,
-    ss.total_sessions_with_search,
-    sd.session_avg_duration
-from total_sessions ts
-left join total_users tu on ts.event_date = tu.event_date
-left join total_new_users tnu on ts.event_date = tnu.event_date
-left join total_page_views tpv on ts.event_date = tpv.event_date
-left join search_sessions ss on ts.event_date = ss.event_date
-left join average_session_duration sd on ts.event_date = sd.event_date
+    ad.event_date,
+    MAX(ad.total_sessions) as total_sessions,
+    MAX(ad.total_users) as total_users,
+    MAX(ad.total_new_users) as total_new_users,
+    MAX(ad.total_page_views) as total_page_views,
+    MAX(ad.total_sessions_with_search) as total_sessions_with_search,
+    asd.session_avg_duration
+from aggregated_data ad
+left join average_session_duration asd on ad.event_date = asd.event_date
+group by ad.event_date, asd.session_avg_duration
