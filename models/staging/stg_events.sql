4c070ef2df2a34d22ba42b52078ca1244b4749e1
pragyaagrawal.au@gmail.com <pragyaagrawal.au@gmail.com>
2024-07-16 17:45:22 +0000

    yml file configured. tests added

--- models/staging/stg_events.sql
+++ models/staging/stg_events.sql
@@ -1,20 +1,26 @@
--- Get the data from source data
+
+{{ config(
+  materialized='table'
+) }}
+
+
 with stg_session_source as (
-    select
-        *
+    select 
+    *
     from {{ source('ga4_obfuscated_sample_ecommerce', 'events_*') }}
 ),
--- select relevant columns from source data
+
 stg_events as (
-    select
+    select 
         user_pseudo_id,
-        CAST(event_date AS DATE) as event_date,
+        CAST(FORMAT_DATE('%Y-%m-%d', PARSE_DATE('%Y%m%d', event_date)) AS DATE) as event_date,
         event_timestamp,
         event_previous_timestamp,
-        event_name,
+        event_name AS event_name,
         user_first_touch_timestamp,
         case
-            when lower(device.category) in ('desktop', 'mobile', 'tablet') then lower(device.category) else 'unknown'
+            when lower(device.category) in ('desktop', 'mobile', 'tablet') then lower(device.category)
+            else 'unknown'
         end as device_category,
         geo.country,
         geo.region,
@@ -22,11 +28,12 @@ stg_events as (
         traffic_source.medium,
         traffic_source.source,
         traffic_source.name,
-        -- Create a unique session_id by combining user_pseudo_id and event_timestamp
-        user_pseudo_id || '-' || event_timestamp as session_id
+        user_pseudo_id || '-' || event_timestamp as session_id 
     from stg_session_source
-    where user_pseudo_id is not null -- Exclude rows with null user_pseudo_id
-        and event_timestamp is not null -- Exclude rows with null event_timestamp
+    where user_pseudo_id is not null  
+        and event_timestamp is not null  
 )
--- Displaying the source data
+
 select * from stg_events
+
+
